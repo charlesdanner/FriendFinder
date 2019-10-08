b9904d488eb77c35799e21c34042281bb5e2bcbd
Charles Danner <danner.ced1@gmail.com>
2019-10-08 15:46:16 -0400

    server works and pressing the link to the survey brings up the survey page.

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});

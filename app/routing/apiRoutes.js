e81888ee8477b041ee98556af7c6bea9a3560130
Charles Danner <danner.ced1@gmail.com>
2019-10-11 11:37:52 -0400

    added a filter so users can't be matched with themselves if they take the survey more than once.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,11 +7,19 @@ module.exports = app => {                                //exporting the express
 
     app.post('/survey', function (req, res) {               //function that runs when a post is made
         
+        const filteredArr = []
+        const filterFriends = (friend) =>{
+            if(friend.name !== req.body.newFriend.name){        //filters the friendsList so that you can't be matched with a user with the same name as you. 
+                filteredArr.push(friend)                        //That's either you or it would be creepy to be friends with them
+            }
+        }
+        friendsList.filter(filterFriends)
+
         const matchValues = [];
-        for(var i = 0; i < friendsList.length; i++){            //for loops that loop through the different people in the array and then loop through the scores they submitted
+        for(var i = 0; i < filteredArr.length; i++){            //for loops that loop through the different people in the array and then loop through the scores they submitted
             let sum = 0;
             for(var j = 0; j < 10; j++){
-                 sum += (Math.abs(req.body.newFriend.scores[j] - friendsList[i].scores[j]))     //gets the absolute value of the difference between the client's score and the potential match's scores at each index and sums them into one total
+                 sum += (Math.abs(req.body.newFriend.scores[j] - filteredArr[i].scores[j]))     //gets the absolute value of the difference between the client's score and the potential match's scores at each index and sums them into one total
             }
             matchValues.push(sum)
         }
